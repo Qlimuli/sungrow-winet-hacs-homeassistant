ebad4742f22850e1cc6d70b181bd9591851fae04
Qlimuli <59221558+Qlimuli@users.noreply.github.com>
2026-01-03 15:23:03 +0100

    Update __init__.py

--- custom_components/sungrow_winet_s/__init__.py
+++ custom_components/sungrow_winet_s/__init__.py
@@ -19,7 +19,6 @@ _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Sungrow WINET-S from a config entry."""
     hass.data.setdefault(DOMAIN, {})
@@ -40,7 +39,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     return True
 
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
@@ -48,7 +46,6 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         await coordinator.async_shutdown()
 
     return unload_ok
-
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Reload config entry."""
